56a85161eb3aea1862cc2156c37609f22b16db97
Jerry Wang <doghost111@gmail.com>
2026-06-24 10:25:21 +0800

    Initial Commit

---
+++ Pikmin_Bloom/Pikmin_BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pikmin_BloomApp.swift
+//  Pikmin_Bloom
+//
+//  Created by Home on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Pikmin_BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
